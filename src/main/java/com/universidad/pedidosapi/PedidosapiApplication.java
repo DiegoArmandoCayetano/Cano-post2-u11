348ba211c9fdc09ba178ae52db6366e4fbd804d8
Diego Armando <160793240+DiegoArmandoCayetano@users.noreply.github.com>
2026-05-13 12:16:42 -0500

    crear proyecto

---
+++ src/main/java/com/universidad/pedidosapi/PedidosapiApplication.java
@@ -0,0 +1,13 @@
+package com.universidad.pedidosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosapiApplication.class, args);
+	}
+
+}
